3b691bca385d0e91b5a2997a8b26a246177eeb82
JAPPLET <trianajuan970@gmail.com>
2024-09-28 16:09:35 -0500

    :bookmark: Add our values

--- src/helpers/DynamicIcon.tsx
+++ src/helpers/DynamicIcon.tsx
@@ -6,7 +6,7 @@ import * as FaIcons from "react-icons/fa6";
 // import * as FiIcons from "react-icons/fi";
 // import * as Io5Icons from "react-icons/io5";
 // import * as RiIcons from "react-icons/ri";
-// import * as TbIcons from "react-icons/tb";
+import * as TbIcons from "react-icons/tb";
 // import * as TfiIcons from "react-icons/tfi";
 
 type IconMap = Record<string, IconType>;
@@ -18,6 +18,7 @@ interface IDynamicIcon extends React.SVGProps<SVGSVGElement> {
 
 const iconLibraries: { [key: string]: IconMap } = {
   fa: FaIcons,
+  tb: TbIcons,
 };
 
 const DynamicIcon: FC<IDynamicIcon> = ({ icon, ...props }) => {
